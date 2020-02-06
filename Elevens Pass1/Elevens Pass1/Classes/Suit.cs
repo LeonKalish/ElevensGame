734cc091ba08cc8beec46294b889c5799af15918
LeonKalish <335747770@gapps.yrdsb.ca>
2020-02-06 12:47:39 -0500

    Added project files

---
+++ Elevens Pass1/Elevens Pass1/Classes/Suit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevens_Pass1.Classes
+{
+    class Suit
+    {
+        public const int HEART = 0;
+        public const int SPADE = 1;
+        public const int DIAMOND = 2;
+        public const int CLUB = 3;
+
+
+    }
+}
